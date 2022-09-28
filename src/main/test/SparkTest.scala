dcb1d080bc28a67e5c4ca350942436f115ecc382
liyanfang <844606704@qq.com>
2022-09-28 22:11:30 +0800

    three commit

---
+++ src/main/test/SparkTest.scala
@@ -0,0 +1,10 @@
+import com.alibaba.fastjson.{JSON, JSONObject}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SparkTest {
+    def main(args: Array[String]): Unit = {
+    }
+}
+
---
+++ src/main/test/TestJedis.java
@@ -0,0 +1,43 @@
+import cn.itcast.service.GetDataService;
+import cn.itcast.util.JedisUtil;
+import redis.clients.jedis.Jedis;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class TestJedis {
+
+    public static void main(String[] args) {
+
+        Jedis jedis = JedisUtil.getJedis();
+
+       /* Map<String, String> testData = jedis.hgetAll("bussiness::order::total");
+
+
+
+        String [] produceId = new String [10];
+        String [] producetSumPrice = new String [10];
+
+        int i=0;
+        for(Map.Entry<String,String> entry : testData.entrySet()){
+            produceId[i]=entry.getKey();
+            producetSumPrice[i] =entry.getValue();
+            i++;
+        }
+
+        UiBean ub = new UiBean();
+        ub.setProducetSumPrice(producetSumPrice);
+        ub.setProduceId(produceId);
+
+        System.out.println(ub);*/
+
+
+        GetDataService getDataService = new GetDataService();
+        String data = getDataService.getData();
+        System.out.println(data);
+
+
+    }
+}
